988695b71ea85b5488957b7a97ea45b6b9e3e46b
kevin li <lizhoayu1@redcreation.cn>
2024-04-02 23:37:58 +0800

    04.04

--- module_04/lesson_04.04/protected/source_code/tinycanva/src/app/core.cljs
+++ module_04/lesson_04.04/protected/source_code/tinycanva/src/app/core.cljs
@@ -1,10 +1,9 @@
 (ns app.core
   "This namespace contains your application and is the entrypoint for 'yarn start'."
   (:require [reagent.core :as r]
-            ["react-router-dom" :refer (BrowserRouter Switch Route)]
-            ["@blueprintjs/core" :refer (Icon)]
-            ))
-
+            [reagent.dom :as dom]
+            ["react-router-dom" :refer (BrowserRouter Routes Route)]
+            ["@blueprintjs/core" :refer (Icon)]))
 
 (defn placeholder [title]
   [:h1 title])
@@ -12,28 +11,28 @@
 (defn test-ui []
   [:div.w-full.h-screen.flex
    [:div.m-auto.text-center
-    [:> Icon {:icon "star" :class "text-yellow-500 mb-2" :iconSize 24}]
-    [:div.text-lg "Tinycanva"]
+    [:> Icon {:icon "star" :class "text-blue-500 mb-2" :iconSize 44}]
+    [:div.text-2xl "Tinycanva"]
     ]])
 
 
 (defn root-router []
   [:> BrowserRouter
-   [:> Switch
-    [:> Route {:exact true :path "/"} [placeholder "Index"]]
-    [:> Route {:path "/login"} [placeholder "Login"]]
-    [:> Route {:path "/register"} [placeholder "Register"]]
-    [:> Route {:path "/graphics"} [placeholder "Graphics List"]]
-    [:> Route {:path "/test-ui"} [test-ui]]
+   [:> Routes
+    [:> Route {:path "/" :element   (r/as-element [placeholder "Indexh"])} ]
+    [:> Route {:path "/login" :element (r/as-element [placeholder "Login"])}]
+    [:> Route {:path "/register" :element (r/as-element [placeholder "Register"])}]
+    [:> Route {:path "/graphics" :element (r/as-element [placeholder "Graphics List"])}]
+    [:> Route {:path "/test-ui" :element (r/as-element [test-ui])}]
 
     ;; catch all
-    [:> Route [placeholder "404 Not Found"]]
+    [:> Route {:path "*" :element (r/as-element [placeholder "404 Not Found"])}]
     ]])
 
 (defn ^:dev/after-load render
   "Render the toplevel component for this app."
   []
-  (r/render [root-router] (.getElementById js/document "app")))
+  (dom/render [root-router] (.getElementById js/document "app")))
 
 (defn ^:export main
   "Run application startup logic."
